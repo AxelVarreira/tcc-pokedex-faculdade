357ec9d9715f13a09cc0fd172cf4f43f3a3b5010
AxelVarreira <axel.felix.varreira2@gmail.com>
2021-08-20 16:13:13 -0300

    first commit

---
+++ src/test/java/com/axel/joao/tcc/poo2/PokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axel.joao.tcc.poo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
