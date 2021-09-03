fa3be249f51273ab6204df25f7b1dba567fd0735
João Zimmer <joaozimmr@gmail.com>
2021-09-03 19:13:34 -0300

    Implementando MainScreen e PokemonCard

---
+++ src/main/java/com/axel/joao/tcc/poo2/gui/MainScreen.java
@@ -0,0 +1,15 @@
+package com.axel.joao.tcc.poo2.gui;
+
+import javax.swing.*;
+
+public class MainScreen {
+    private JPanel mainPanel;
+    private JTabbedPane tabbedPane1;
+    private JPanel JP_user;
+    private JPanel JP_search;
+    private JPanel JP_nome;
+    private JScrollBar scrollBar1;
+    private JTextField textField1;
+    private JButton BT_search;
+
+}
